f95393fca3eb2eb99faf9642566daee335ed8ba3
Sulaksana16 <sulaksana16@gmail.com>
2020-05-15 04:47:32 +0700

    Navbar belom selesai setiap ditambahkan eror

---
+++ client/src/components/Dash.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="dash">
+<b-button variant="outline-primary" v-on:click="addRoom">Create Room</b-button>
+<b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+    <b-dropdown-item>Room1</b-dropdown-item>
+    <b-dropdown-item>Room2</b-dropdown-item>
+    <b-dropdown-item>Room3</b-dropdown-item>
+  </b-dropdown>
+<b-button variant="success" v-on:click="joint">Joint</b-button>
+<b-button variant="danger" id="logout" v-on:click="logOut">Log Out</b-button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Dash',
+  methods: {
+    addRoom () {
+      this.$router.push({ path: '/add-room' })
+    },
+    joint () {
+      this.$router.push({ path: '/main' })
+    },
+    logOut () {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
+</script>
+
+<style>
+#dash{
+display: flex;
+justify-content: center;
+align-items: center;
+}
+#logout{
+margin-left: 10px;
+}
+</style>
---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:header>Snake and Ladder</template>
+
+      <template v-slot:lead>
+        This is such a simple game
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+       Go!! LogIn, release your stressed !!
+      </p>
+      <b-button variant="primary" href="#" id="login" v-on:click="LogIn">Log In</b-button>
+      <b-button variant="success" href="#" id="register" v-on:click="Register">Register</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  methods: {
+    LogIn () {
+      this.$router.push({ path: '/login' })
+    },
+    Register () {
+      this.$router.push({ path: '/register' })
+    }
+  }
+}
+</script>
+
+<style>
+#login{
+  margin-right : 20px;
+}
+</style>
